ca5188fece0333d0e768686cb32085e26837dd02
aflores614 <andres.qq.flores@gmail.com>
2024-08-28 18:07:18 -0700

    update

---
+++ lora.py
@@ -0,0 +1,41 @@
+import serial
+import time
+
+
+
+def send_command(ser, ADDRESS, command):
+    command_len = str(len(command))
+    c = ('ALT+SEND=' + str(ADDRESS) + ',' + command_len + ',' + command + '\r\n')
+    print(c)
+    ser.write(('AT+SEND=' + str(ADDRESS) + ',' + command_len + ',' + command + '\r\n').encode())
+    time.sleep(0.5)  # Wait for the command to be processed
+def get_address(ser):
+    ser.write(('AT+ADDRESS?\r\n').encode())  # Send the AT command to query the address
+    time.sleep(0.5)  # Wait for the response
+    response = ser.read_all().decode()  # Read and decode the response
+    if response.startswith('+'):  # Check if the response is valid
+        parts = response.split('=')  # Split the response at the '=' character
+
+        return parts[1].strip()  # Return the address (after '='), stripped of any whitespace
+def get_network(ser):
+    ser.write(('AT+NETWORKID?\r\n').encode())  # Send the AT command to query the address
+    time.sleep(0.5)  # Wait for the response
+    response = ser.read_all().decode()  # Read and decode the response
+    if response.startswith('+'):  # Check if the response is valid
+        parts = response.split('=')  # Split the response at the '=' character
+        
+        return parts[1].strip()  # Return the address (after '='), stripped of any whitespace
+
+def read_command(ser):
+    while True:
+        string = ser.readline()
+        c = string.decode("utf-8")
+        if(c != ""):
+            if c.startswith('+RCV='):
+                parts = c.split(',')
+                sender_id = parts[0].split('=')[1]  
+                message_length = parts[1]           
+                message_payload = parts[2]          
+                rssi = parts[3]                     
+                snr = parts[4]   
+                return message_payload
--- main.py
+++ main.py
@@ -1,59 +1,51 @@
 import serial
 import time
 from connect_to_vehicle import connect_to_vehicle
+from arm_drone import arm_drone
+from arm_drone import is_armed
+from disarm_drone import disarm_drone
+from lora import send_command,get_address,get_network,read_command
 
 serial_port = '/dev/ttyUSB1'
 baud_rate = 115200  # Default baud rate for RYLR998
 
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
-def send_command(ADDRESS, command):
-    command_len = str(len(command))
-    c = ('ALT+SEND=' + str(ADDRESS) + ',' + command_len + ',' + command + '\r\n')
-    print(c)
-    ser.write(('AT+SEND=' + str(ADDRESS) + ',' + command_len + ',' + command + '\r\n').encode())
-   # time.sleep(0.5)  # Wait for the command to be processed
-def get_address():
-    ser.write(('AT+ADDRESS?\r\n').encode())  # Send the AT command to query the address
-    time.sleep(0.5)  # Wait for the response
-    response = ser.read_all().decode()  # Read and decode the response
-    if response.startswith('+'):  # Check if the response is valid
-        parts = response.split('=')  # Split the response at the '=' character
-        print(parts)  # Print the parts for debugging
-        return parts[1].strip()  # Return the address (after '='), stripped of any whitespace
-def get_network():
-    ser.write(('AT+NETWORKID?\r\n').encode())  # Send the AT command to query the address
-    time.sleep(0.5)  # Wait for the response
-    response = ser.read_all().decode()  # Read and decode the response
-    if response.startswith('+'):  # Check if the response is valid
-        parts = response.split('=')  # Split the response at the '=' character
-        print(parts)  # Print the parts for debugging
-        return parts[1].strip()  # Return the address (after '='), stripped of any whitespace
+try:
+    ADDRESS = get_address(ser)
+    print("The ADDRESS is: ", ADDRESS)
+    network = get_network(ser)
+    print("The network is: ", network)
+    send_command(ser, ADDRESS, "Connection Test y/n")
 
-def read_command():
-	while True:
-	
-		string = ser.readline()
-		c =string.decode("utf-8")
-		if( c != ""):
-			if c.startswith('+RCV='):
-				parts = c.split(',')
-				sender_id = parts[0].split('=')[1]  
-				message_length = parts[1]           
-				message_payload = parts[2]          
-				rssi = parts[3]                     
-				snr = parts[4]                      
-            
-            # Print the separated message components				
-				print(f"Message: {message_payload}")
-				print(f"RSSI: {rssi}")
+    while True:
+        reponse = read_command(ser)
+        if(reponse == 'y'):
+            break
+        else:
+            time.sleep(0.5)
+    master = connect_to_vehicle()
+    if master:
+        send_command(ser, ADDRESS, "Vehicle Connect")
+        send_command(ser, ADDRESS,"Arm the Drone y/n")
+        while True:
+            arm_response = read_command(ser)
+            if(arm_response == 'y'):
+                break
+            else:
+                time.sleep(0.5)
+        arm_drone(master)
+        while True:
+            if is_armed(master):
+                send_command(ser, ADDRESS, "Drone is armed!")
+                break            
+            else:
+                send_command(ser, ADDRESS,"Drone is not armed")      
+                time.sleep(1)
+        time.sleep(5)
+        disarm_drone(master)
+except KeyboardInterrupt:  
+    print("Can't connect")
 
-
-
-ADDRESS = get_address()
-print("The ADDRESS is: ", ADDRESS)
-network = get_network()
-print("The network is: ", network)
-send_command(ADDRESS, "Andres")
  
 ser.close()
